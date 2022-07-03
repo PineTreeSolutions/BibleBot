56dd71cb938bb7cee6ee5eb2dfebf028225acc87
NuclearPine <nuclearpine.h3nbw@aleeas.com>
2022-07-02 20:42:24 -0600

    verse retrieval implemented

--- tg_biblebot/bible.py
+++ tg_biblebot/bible.py
@@ -22,17 +22,15 @@ def regex(input):
     x = re.search('^[1-9]?[a-zA-Z ]+', input)
     if x:
         ref['book'] = x.group().lower().rstrip()
-        print('Book: ' + ref['book'])
         input = input[x.span()[1]:]
     else:
-        return('You need to specify a book')
+        return('Invalid syntax: book name')
 
     # find chapter
     input = re.sub('^:', '', input).lstrip()
     x = re.search('^[1-9]+[0-9]*', input)
     if x:
-        ref['chapter'] = x.group()
-        print('Chapter: ' + ref['chapter'])
+        ref['chapter'] = int(x.group())
         input = input[x.span()[1]:].lstrip()
     else:
         return('Invalid syntax: chapter number')
@@ -41,7 +39,7 @@ def regex(input):
     input = re.sub('^:', '', input).lstrip()
     x = re.search('^[1-9]+[0-9]*', input)
     if x: #case 2, 3
-        ref['verse'] = x.group()
+        ref['verse'] = int(x.group())
         ref['exact'] = True
         input = input[x.span()[1]:].lstrip()
     elif input == '': # case 1
@@ -53,18 +51,38 @@ def regex(input):
     # find ending verse
     x = re.search('^-[1-9]+[0-9]*$', input)
     if x: # case 3
-        ref['verse_end'] = re.sub('^-', '', x.group())
-        print('End verse: ' + ref['verse_end'])
+        ref['verse_end'] = int(re.sub('^-', '', x.group()))
         return ref
     elif input == '': # case 2
         ref['verse_end'] = ref['verse']
-        print('End verse: ' + ref['verse_end'])
         return ref
     else:
         return('Invalid syntax: verse number')
 
-def fetch_verses(input): #WIP
+def fetch_verses(input):
     ref = regex(input)
     if type(ref) != dict:
         return ref
 
+    book_num = 0
+    if kjv.book_abr.eq(ref['book']).any():
+        book_num = kjv[kjv.book_abr == ref['book']].iloc[0].book_num
+    elif kjv.book.eq(ref['book']).any():
+        book_num = kjv[kjv.book == ref['book']].iloc[0].book_num
+    else:
+        return 'Invalid book name'
+    df = kjv[kjv.book_num == book_num]
+
+    if not df.chapter.eq(ref['chapter']).any():
+        return 'Invalid chapter'
+    df = df[df.chapter == ref['chapter']]
+
+    if not (df.verse.eq(ref['verse']).any() & df.verse.eq(ref['verse_end']).any()):
+        return 'Invalid verse selection'
+    verses = df[(df.verse >= ref['verse']) & (df.verse <= ref['verse_end'])]
+
+    message = ''
+    for i, j in zip(verses.verse, verses.text):
+        message += f'<b>{i}</b>    {j}\n'
+    
+    return message
